a4604861183c0796fb1f0c40e264c258ab139f77
Ilie Alexandru <alexandruilie@yahoo.com>
2014-04-06 13:55:44 +0300

    AddHomework

---
+++ Homework/Homework/Controllers/AddHomeworkController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework.Controllers
+{
+    public class AddHomeworkController : Controller
+    {
+        //
+        // GET: /AddHomework/
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
